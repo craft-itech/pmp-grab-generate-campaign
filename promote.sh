7b8bb5ee2c10c01ab57c72c9d33fa645632eaddf
Tantai Thanakanok <leknarm@gmail.com>
2025-03-06 18:09:52 +0700

    fix: wrong container name

--- promote.sh
+++ promote.sh
@@ -10,13 +10,16 @@ tag=$(<version)
 
 if [ $env = "dev" ]; then
     echo "Promoting version $tag to pmpactive-dev"
-    kubectl set image deploy/pmp-promotion-grabmart-producer pmp-promotion-grabmart-producer=cgacraksnonprd.azurecr.io/pmp/pmp-promotion-grabmart-producer:"$tag" -n pmpactive-dev
+    kubectl set image deploy/pmp-promotion-grabmart-producer pmp-promotion-consumer=cgacraksnonprd.azurecr.io/pmp/pmp-promotion-grabmart-producer:"$tag" -n pmpactive-dev
+    kubectl set image deploy/pmp-promotion-grabmart-producer-2 pmp-promotion-consumer=cgacraksnonprd.azurecr.io/pmp/pmp-promotion-grabmart-producer:"$tag" -n pmpactive-dev
 elif [ $env = "sit" ]; then
     echo "Promoting version $tag to pmpactive-sit"
-    kubectl set image deploy/pmp-promotion-grabmart-producer pmp-promotion-grabmart-producer=cgacraksnonprd.azurecr.io/pmp/pmp-promotion-grabmart-producer:"$tag" -n pmpactive-sit
+    kubectl set image deploy/pmp-promotion-grabmart-producer pmp-promotion-consumer=cgacraksnonprd.azurecr.io/pmp/pmp-promotion-grabmart-producer:"$tag" -n pmpactive-sit
+    kubectl set image deploy/pmp-promotion-grabmart-producer-2 pmp-promotion-consumer=cgacraksnonprd.azurecr.io/pmp/pmp-promotion-grabmart-producer:"$tag" -n pmpactive-sit
 elif [ $env = "uat" ]; then
     echo "Promoting version $tag to pmpactive-uat"
-    kubectl set image deploy/pmp-promotion-grabmart-producer pmp-promotion-grabmart-producer=cgacraksnonprd.azurecr.io/pmp/pmp-promotion-grabmart-producer:"$tag" -n pmpactive-uat
+    kubectl set image deploy/pmp-promotion-grabmart-producer pmp-promotion-consumer=cgacraksnonprd.azurecr.io/pmp/pmp-promotion-grabmart-producer:"$tag" -n pmpactive-uat
+    kubectl set image deploy/pmp-promotion-grabmart-producer-2 pmp-promotion-consumer=cgacraksnonprd.azurecr.io/pmp/pmp-promotion-grabmart-producer:"$tag" -n pmpactive-uat
 else
     echo "Invalid environment"
     exit 1
